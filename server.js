9f5021c0b2567d090f00889a157a6e2d11e6e9ce
Yan Lucas <88909517+yanswo@users.noreply.github.com>
2025-02-01 17:42:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
 const middlewares = jsonServer.defaults();
+
+// Configuração para a variável de ambiente PORT
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 
+// Roteamento
 server.use(
   jsonServer.rewriter({
-    "/*": "/$1",
+    "/*": "/$1", // Garantir que a API seja reescrita corretamente
   })
 );
 
+// Uso do router com o db.json
 server.use(router);
 
-const port = process.env.PORT || 3000;
+// Configuração da porta para o servidor, o Render deve passar o port via process.env.PORT
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
