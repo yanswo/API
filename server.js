1382c0d16debce0ad79e89773ba4b1d81200387c
Yan Lucas <88909517+yanswo@users.noreply.github.com>
2025-02-01 19:01:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const jsonServer = require("json-server");
+const cors = require("cors"); // Importando o CORS
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 
 const middlewares = jsonServer.defaults();
 
+// Usando CORS para permitir requisições de outros domínios
+server.use(cors());
+
 server.use(middlewares);
 
 server.use(
   jsonServer.rewriter({
-    "/*": "/$1",
+    "/*": "/$1", // Reescrevendo a URL para o formato correto
   })
 );
 
 server.use(router);
 
-// Usa a variável de ambiente PORT fornecida pela plataforma
-const port = process.env.PORT || 3000; // A plataforma define a porta, ou 3000 como fallback
+// Usa a variável de ambiente PORT fornecida pela plataforma ou 3000 como fallback
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
